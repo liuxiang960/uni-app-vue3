e0f7b4e4b0529a1dd706b2d40313bf6ef8af4db0
kuangw <kuangw11@gmail.com>
2023-03-03 17:53:54 +0800

    初始化模板

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+onLaunch(() => {
+  console.log("App Launch");
+});
+onShow(() => {
+  console.log("App Show");
+
+});
+onHide(() => {
+
+});
+</script>
+<style></style>
